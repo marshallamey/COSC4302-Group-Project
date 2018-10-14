bc9872136bebab12cc62a19eff8263cdd1143dd3
kpatel20538 <kpatel20538@gmail.com>
2018-10-14 00:51:39 +0000

    Update Main.h

--- Main.h
+++ Main.h
@@ -1,5 +1,10 @@
+#include <string>
+#include <vector>
 
 class Command {
+  std::string filename;
+  std::vector<std::string> args;
+public:
   Command(std::string filename, std::vector<std::string> args);
   invoke();
 }
