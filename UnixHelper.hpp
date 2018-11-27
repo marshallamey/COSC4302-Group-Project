8bd6c1eae1527ac44c109498d061a78a8d2054da
kpatel20538 <kpatel20538@gmail.com>
2018-11-27 17:44:56 +0000

    Create UnixHelper.hpp

---
+++ UnixHelper.hpp
@@ -0,0 +1,12 @@
+#ifndef UNIX_HELPER_H
+#define UNIX_HELPER_H
+
+#include <string>
+
+// Get CS Path for Path Resolution
+std::string get_cs_path();
+
+// Get Current Working Directory
+std::string get_cwd();
+
+#endif // UNIX_HELPER_H
