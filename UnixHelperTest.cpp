6285ef1e5f32dbf309772796a7cc6364bd69a709
kpatel20538 <kpatel20538@gmail.com>
2018-11-27 17:47:00 +0000

    Update UnixHelperTest.cpp

--- UnixHelperTest.cpp
+++ UnixHelperTest.cpp
@@ -1,6 +1,10 @@
 #include "UnixHelper.hpp"
 #include <iostream>
 
+// To run test, run the following commands
+// $ gcc -std=c++11 -o unix-test UnixHelper.hpp UnixHelper.cpp UnixHelperTest.cpp
+// $ ./unix-test
+
 int main () {
     std::cout << get_cs_path() << '\n';
     std::cout << get_cwd() << '\n';
