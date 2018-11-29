e6eb024365d27218f696e66054cebb67c821eb0b
kpatel20538 <kpatel20538@gmail.com>
2018-11-29 20:55:57 +0000

    Updated UnixHelper with two new functions

--- UnixHelperTest.cpp
+++ UnixHelperTest.cpp
@@ -6,6 +6,11 @@
 // $ ./unix-test
 
 int main () {
-    std::cout << get_cs_path() << '\n';
-    std::cout << get_cwd() << '\n';
+    std::cout << get_cs_path() << '\n';       // Expected: /bin:/usr/bin
+    std::cout << get_cwd() << '\n';           // Expected: /home/username/workspace
+    std::cout << exists("/notafile");         // Expected: 0
+    std::cout << exists("/proc/version");     // Expected: 1
+    std::cout << executable("/proc/version"); // Expected: 0
+    std::cout << executable("/bin/echo");     // Expected: 1
+    std::cout << '\n';
 }
