ecf538491086ab793d608454aa449e948e05e65d
kpatel20538 <kpatel20538@gmail.com>
2018-11-29 20:53:21 +0000

    STUBBED version. Requires Implementation

---
+++ Parser.cpp
@@ -0,0 +1,17 @@
+#include "Parser.hpp"
+
+// This is a stubbed version of the to be implemented Parser.cpp source file. 
+
+std::vector<std::string> tokenize(const std::string& document, char delimiter) {
+    return std::vector<std::string> { }; 
+}
+
+std::string path_resolver(const std::string& program_name) {
+    return std::string { };
+}
+
+Command make_command(const std::string& document) {
+    return Command {{
+        "/bin/echo", "hello", "world"
+    }};
+}
