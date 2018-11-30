f029e4281e37e6df045aa385ea0f5464ef7669fa
kpatel20538 <kpatel20538@gmail.com>
2018-11-30 22:59:42 +0000

    Added filename and default constructor

--- Command.hpp
+++ Command.hpp
@@ -1,22 +1,31 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
-#include <string>
 #include <vector>
+#include <string>
 
-// This Command Object repesent a Executable UNIX Program with pre-specfied arguments
-//
-// Note this command object will spin up a child process, which may 
-// fail to execute the program In that case, a ExecutionException will thrown
-// Catch in the main function and terminate the program  to avoid having 
-// multiple shell processes inquiring user input
-// The main process will be unaffected as ExecutionException will only
-// be thrown in the child process 
-
+/* This Command Object repesent a Executable UNIX Program with pre-specfied arguments
+ *
+ * Note this command object will spin up a child process, which may 
+ * fail to execute the program In that case, a ExecutionException will thrown
+ * Catch in the main function and terminate the program  to avoid having 
+ * multiple shell processes inquiring user input
+ * The main process will be unaffected as ExecutionException will only
+ * be thrown in the child process 
+ *
+ * Also note that this object has no mutator methods. Meaning once the 
+ * filename and arguments are set, they can not be altered. This was done 
+ * prevent the Command object from ever being in a invalid state.
+ */
+ 
 class Command {
+    std::string filename;
     std::vector<std::string> args;
 public:
-    explicit Command(const std::vector<std::string>& args);
+    Command(const std::string& filename ,const std::vector<std::string>& args);
+    Command();
+    
+    const std::string& get_filename() const;
     const std::vector<std::string>& get_args() const;
     void execute(); // throws(ExecutionException)
 };
