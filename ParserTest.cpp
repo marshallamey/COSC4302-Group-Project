8afef452d6f07eb836282fd9d79980bf67577f88
kpatel20538 <kpatel20538@gmail.com>
2018-11-30 23:25:23 +0000

    Remove local script tests, added filename tests

--- ParserTest.cpp
+++ ParserTest.cpp
@@ -1,12 +1,11 @@
 #include "Parser.hpp"
-#include "UnixHelper.hpp"
 #include "Command.hpp"
 #include <iostream>
 
-// To run test, run the following commands
+// To execute run the following commands
 // $ make ParserTest
 
-// Expected: 1111111111111
+// Expected: 1111 111 11111111
 int main() {
     // Test 1a: Tokenize space-delimted string
     std::vector<std::string> args1 = 
@@ -18,15 +17,13 @@ int main() {
 
     // Test 1b: Tokenize empty string
     std::vector<std::string> args2 = tokenize("", ' ');
-    std::vector<std::string> expected_args2 { };   
-    std::cout << (args2 == expected_args2);
+    std::cout << args2.empty();
     
     // Test 1c: Tokenize space-only string
     std::vector<std::string> args3 = tokenize("   ", ' ');
-    std::vector<std::string> expected_args3 { };   
-    std::cout << (args3 == expected_args3);
+    std::cout << args3.empty();
 
-    // Test 2: Tokenize colon-delimted string
+    // Test 1d: Tokenize colon-delimted string
     std::vector<std::string> paths = 
         tokenize("/bin:/sbin:::/usr/bin:::/usr/sbin/::::", ':');
     std::vector<std::string> expected_paths {
@@ -34,54 +31,55 @@ int main() {
     };
     std::cout << (paths == expected_paths);
 
-    // Test 3a: Path Resolve for /bin
+    std::cout << ' ';
+    
+    // Test 2a: Path Resolve for /bin
     std::string echo_path = path_resolver("echo");
     std::cout << (echo_path == "/bin/echo");
 
-    // Test 3b: Path Resolve for /usr/bin
+    // Test 2b: Path Resolve for /usr/bin
     std::string find_path = path_resolver("find");
     std::cout << (find_path == "/usr/bin/find");
     
-    // Test 3c: Path Resolve for local script
-    std::string myscript_path = path_resolver("myscript");
-    std::cout << (myscript_path == (get_cwd() + "/myscript"));
-    
-    // Test 3d: Path Resolve for slashed command
+    // Test 2c: Path Resolve for slashed command
     std::string ls_path = path_resolver("/bin/ls");
     std::cout << (ls_path == "/bin/ls");
     
-    // Test 4a: Echo Command
+    std::cout << ' ';
+    
+    // Test 3a: Echo Command
+    std::string expected_echo_filename { "/bin/echo" };
+    std::vector<std::string> expected_echo_args { "echo", "hello", "world" };
+    
     Command echo = make_command(" echo  hello   world   ");
-    std::vector<std::string> expected_echo_args {
-        "/bin/echo", "hello", "world"
-    };
+    
+    std::cout << (echo.get_filename() == expected_echo_filename);
     std::cout << (echo.get_args() == expected_echo_args);
 
-    // Test 4b: Find Command
+    // Test 3b: Find Command
+    std::string expected_find_filename { "/usr/bin/find" };
+    std::vector<std::string> expected_find_args { "find", "." };
+    
     Command find = make_command("find     .  ");
-    std::vector<std::string> expected_find_args {
-        "/usr/bin/find", "."
-    };
+    
+    std::cout << (find.get_filename() == expected_find_filename);
     std::cout << (find.get_args() == expected_find_args);
     
-    // Test 4c: Local Script Command
-    Command myscript = make_command("myscript");
-    std::vector<std::string> expected_myscript_args {
-        get_cwd() + "/myscript"
-    };
-    std::cout << (myscript.get_args() == expected_myscript_args);
+
+    // Test 3c: Slash Command
+    std::string expected_ls_filename { "/bin/ls" };
+    std::vector<std::string> expected_ls_args { "/bin/ls", "." };
     
-    // Test 4d: Slash Command
     Command ls = make_command("/bin/ls     .  ");
-    std::vector<std::string> expected_ls_args {
-        "/bin/ls", "."
-    };
+    
+    std::cout << (ls.get_filename() == expected_ls_filename);
     std::cout << (ls.get_args() == expected_ls_args);
     
-    // Test 4e: Empty Command
+    // Test 3d: Empty Command
     Command empty = make_command("  ");
-    std::vector<std::string> expected_empty_args { };
-    std::cout << (empty.get_args() == expected_empty_args);
     
+    std::cout << empty.get_filename().empty();
+    std::cout << empty.get_args().empty();
+
     std::cout << '\n';
 }
