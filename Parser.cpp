9b11e3247159c8375c3e3b3e7b74575a7f64a187
Marshall Amey <marshall.amey@gmail.com>
2018-12-01 19:20:25 -0800

    Rough outline of Parser.cpp

--- Parser.cpp
+++ Parser.cpp
@@ -1,17 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
 #include "Parser.hpp"
+#include "Command.hpp"
 
-// This is a stubbed version of the to be implemented Parser.cpp source file. 
 
 std::vector<std::string> tokenize(const std::string& document, char delimiter) {
-    return std::vector<std::string> { }; 
+    std::stringstream ss(document);
+    std::string token;
+    std::vector<std::string> tokens;
+    
+    while( getline(ss, token, delimiter) ) tokens.push_back(token);
+    return tokens; 
 }
 
 std::string path_resolver(const std::string& program_name) {
+
+    // If path starts with a slash, return it as it is
+    if (program_name.at(0) == '/') return program_name;
+
+    // Else search the directories in $PATH environment variable
+    std::vector<std::string> paths = tokenize(string(getenv("PATH"), ':');
     return std::string { };
+    
+    // Else if $PATH not defined, search the directories in CS_PATH config variable
+ 
+    // Else throw exception  
 }
 
 Command make_command(const std::string& document) {
-    return Command {
-        "/bin/echo", {"echo", "hello", "world"}
-    };
+
+    // Parse document to retrieve list of args
+    std::vector<std::string> args = tokenize(document, ' \n\t\r');
+    
+    // Find the path for the file
+    std::string filename = path_resolver(args[0]);
+    
+    // Return a command
+    return Command(filename, args);
 }
