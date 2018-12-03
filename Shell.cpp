506cff3d9d9ae580ce8b1ce79c0555e746dc5c27
theinfamousmrmeow <alexander.d.macey@gmail.com>
2018-12-02 23:51:07 -0600

    Update Shell.cpp

--- Shell.cpp
+++ Shell.cpp
@@ -10,12 +10,18 @@
 #include "Command.hpp"
 #include "UnixHelper.hpp"
 
+#define MAX_ARGS 64
+#define MAX_ARG_LEN 16
+#define MAX_LINE_LEN 128
+#define WHITESPACE ".,\t\n"
 #define PROMPT_CHARACTER ">"
 #define PROMPT_NAME "conch"
 
 //Formats the prompt and outputs to terminal.
 void printPrompt()
 {
+    //TODO:  Build out prompt?
+    //String promptString = "";
     std::string prompt;
     prompt.append(getenv("USER"));
     prompt.append("@");
@@ -26,18 +32,47 @@ void printPrompt()
     std::cout << prompt;
 }
 
+//Trim whitespace
+std::string trim(const std::string &document)
+{
+
+    auto pos = document.find_first_not_of(" \n\t\r");
+
+    auto end_pos = document.find_last_not_of(" \n\t\r");
+
+    if (pos == std::string::npos)
+    {
+
+        return "";
+    }
+
+    else if (end_pos == std::string::npos)
+    {
+
+        return document.substr(pos);
+    }
+
+    else
+    {
+
+        return document.substr(pos, end_pos - pos + 1);
+    }
+}
+
 int main()
 {
     std::string cmdLine;
 
+    //Initialize Shell
+
     //Input Loop
-    while (1)
+    while (std::cin)
     {
 
         printPrompt();
         //Wait for next input
         getline(std::cin, cmdLine);
-
+        cmdLine = trim(cmdLine);
         //Act on input
         //Check for exit command
         if (cmdLine == "exit")
@@ -61,5 +96,5 @@ int main()
             }
         }
     }
-    //We should not reach this.
+    //Only reach this if you killed CIN
 }
